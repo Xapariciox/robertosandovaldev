a566978a87e3e452b4fdfe85eabd946222929c6b
Roberto <cacaracteres86@gmail.com>
2023-01-26 02:35:02 +0100

    inital commit

---
+++ src/infrastructure/components/app/App.tsx
@@ -0,0 +1,3 @@
+export const App = () => {
+    return <h1>Lading page en progreso</h1>;
+};
---
+++ src/infrastructure/components/app/app.test.tsx
@@ -0,0 +1,9 @@
+import '@testing-library/jest-dom';
+import { screen, render } from '@testing-library/react';
+import { App } from './App';
+test('Then it should display the title', () => {
+    render(<App></App>);
+
+    const element = screen.getByText(/progreso/i);
+    expect(element).toBeInTheDocument();
+});
