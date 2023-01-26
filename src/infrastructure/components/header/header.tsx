f7648c43613861b02b506f3e3a41929143fb50cf
Roberto <cacaracteres86@gmail.com>
2023-01-26 20:59:02 +0100

    projects avances

--- src/infrastructure/components/header/header.tsx
+++ src/infrastructure/components/header/header.tsx
@@ -206,6 +206,98 @@ export const Header = () => {
                     </div>
                 </nav>
             </div>
+            <div className="container-fluid">
+                {' '}
+                <div className="row slider">
+                    <div className="col">
+                        <div
+                            className="carousel slide wi"
+                            data-bs-ride="carousel"
+                            id="slider"
+                        >
+                            <div
+                                className={`carousel-inner  ${style.carrousel}  `}
+                            >
+                                <div
+                                    className={`carousel-item ${style.carouselItem} active  `}
+                                    data-bs-interval="3000"
+                                >
+                                    <img
+                                        className={`d-block  img-fluid ${style.carouselImgIpad} `}
+                                        src="../../../assets/meeseks-ipad.png"
+                                        alt=""
+                                    />
+                                </div>
+                                <div
+                                    className={`carousel-item ${style.carouselItem}   `}
+                                    data-bs-interval="3000"
+                                >
+                                    <img
+                                        className={`d-block  img-fluid ${style.carouselImgPhone} `}
+                                        src="../../../assets/meeseks-Phone-2.png"
+                                        alt=""
+                                    />
+                                </div>
+                                <div
+                                    className={`carousel-item ${style.carouselItem}   `}
+                                    data-bs-interval="3000"
+                                >
+                                    <img
+                                        className={`d-block  img-fluid ${style.carouselImgIpad} `}
+                                        src="../../../assets/meeseks-ipad.png"
+                                        alt=""
+                                    />
+                                </div>
+                            </div>
+                            <button
+                                className="carousel-control-rev btn btn-primary"
+                                type="button"
+                                data-bs-target="#slider"
+                                data-bs-slide="prev"
+                            >
+                                <span
+                                    className="carousel-control-prev-icon"
+                                    aria-hidden="true"
+                                ></span>
+                            </button>
+                            <button
+                                className="carousel-control-rev btn btn-primary"
+                                type="button"
+                                data-bs-target="#slider"
+                                data-bs-slide="next"
+                            >
+                                <span
+                                    className="carousel-control-next-icon"
+                                    aria-hidden="true"
+                                ></span>
+                            </button>
+                            <div className="carousel-indicators">
+                                <button
+                                    type="button"
+                                    data-bs-target="#slider"
+                                    data-bs-slide-to="0"
+                                    className="active"
+                                    aria-current="true"
+                                ></button>
+                                <button
+                                    type="button"
+                                    data-bs-target="#slider"
+                                    data-bs-slide-to="1"
+                                    className="active"
+                                    aria-current="true"
+                                ></button>
+                                <button
+                                    type="button"
+                                    data-bs-target="#slider"
+                                    data-bs-slide-to="2"
+                                    className="active"
+                                    aria-current="true"
+                                ></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </header>
     );
 };
