4ce1344d38bd2ce1ab25b95b8541b8fec237dd0d
Alejandro <alejandro@MacBook-Pro-de-TakeAway.local>
2023-03-03 12:30:25 +0100

    arreglo var por const

--- src/infrastructure/components/app/App.tsx
+++ src/infrastructure/components/app/App.tsx
@@ -7,35 +7,35 @@ import { Project } from '../projects/projects';
 export const App = () => {
 
     // animaciones
-    let itemLeft = document.getElementsByClassName("left");
-    let itemRight = document.getElementsByClassName("right");
-    let itemTop = document.getElementsByClassName("top");
-    let itemBottom = document.getElementsByClassName("bottom");
-    let itemOp = document.getElementsByClassName("appear");
+    const itemLeft = document.getElementsByClassName("left");
+    const itemRight = document.getElementsByClassName("right");
+    const itemTop = document.getElementsByClassName("top");
+    const itemBottom = document.getElementsByClassName("bottom");
+    const itemOp = document.getElementsByClassName("appear");
     window.addEventListener('scroll', function () {
-        for (var i = 0; i < itemLeft.length; i++) {
+        for (let i = 0; i < itemLeft.length; i++) {
           if (isInViewport(itemLeft[i])) {
             itemLeft[i].classList.add('animation-left')
           } else if (!isInViewport(itemLeft[i])) {
             itemLeft[i].classList.remove('animation-left')
           }
         }
-        for (var i = 0; i < itemRight.length; i++) {
+        for (let i = 0; i < itemRight.length; i++) {
           if (isInViewport(itemRight[i])) {
             itemRight[i].classList.add('animation-right')
           }
         }
-        for (var i = 0; i < itemBottom.length; i++) {
+        for (let i = 0; i < itemBottom.length; i++) {
           if (isInViewport(itemBottom[i])) {
             itemBottom[i].classList.add('animation-bottom')
           }
         }
-        for (var i = 0; i < itemTop.length; i++) {
+        for (let i = 0; i < itemTop.length; i++) {
           if (isInViewport(itemTop[i])) {
             itemTop[i].classList.add('animation-top')
           }
         }
-        for (var i = 0; i < itemOp.length; i++) {
+        for (let i = 0; i < itemOp.length; i++) {
           if (isInViewport(itemOp[i])) {
             itemOp[i].classList.add('animation-appear')
           }
@@ -47,7 +47,7 @@ export const App = () => {
       function isInViewport(item: any) {
         
       
-        var bounding = item.getBoundingClientRect(),
+        let bounding = item.getBoundingClientRect(),
           myElementHeight = item.offsetHeight,
           myElementWidth = item.offsetWidth;
       
